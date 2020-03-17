fbce631429009883bb0e11e1dbddd997da19564d
tosch314 <facebook.t@gmx.de>
2020-03-17 07:23:47 +0100

    test upload

---
+++ sorting_algorythm.py
@@ -0,0 +1,28 @@
+import random
+
+def random_sort(nums):
+  counter = 0
+  swapped = True
+  while swapped:
+    swapped = False
+    for i in range(len(nums)-1):
+      if nums[i] > nums[i+1]:
+        random.shuffle(nums)
+        counter += 1
+        swapped = True
+        print(counter)
+        print(nums)
+
+
+a = 10
+sophie_list = [2,2,0,6,1,9,9,6]
+random_list = []
+for i in range(0,a):
+  n = random.randint(1,30)
+  random_list.append(n)
+
+
+
+random_sort(sophie_list)
+
+print(sophie_list)
